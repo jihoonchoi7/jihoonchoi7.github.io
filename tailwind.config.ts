b01f3f0a67eaf63011026b641aeeba4dd20dc11e
Your Name <your.email@example.com>
2024-11-14 23:49:16 -0600

    website change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,21 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  		extend: {
+  			colors: {
+  				background: "#f6f9fc",
+  				text: "#000000",
+  			},
+  			fontFamily: {
+  				sans: ["Times New Roman", "serif"],
+  			},
+  		}
   },
   plugins: [],
 };
